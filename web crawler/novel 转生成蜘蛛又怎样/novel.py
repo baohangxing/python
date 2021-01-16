786e263a7bcd27da0def2a483ef63b2e2f9bb9cd
baohangxing <2292398086@qq.com>
2021-01-17 00:29:45 +0800

    [feat] 小说爬虫的优化

--- web crawler/novel 转生成蜘蛛又怎样/novel.py
+++ web crawler/novel 转生成蜘蛛又怎样/novel.py
@@ -1,54 +1,75 @@
 # -*- coding: utf-8 -*-
 # __author__ = 'H1MPLE'
 # __link__ = 'https://github.com/baohangxing/python'
+import time
 import urllib.request
 import re
+import os
 
 
 class TxtWriter:
     # 初始化，传入名字
     def __init__(self, name, ):
         # base链接地址
-        self.name = '.\\' + name + ".txt"
-        self.init()
+        self.name = '.\\' + name
 
-    def init(self):
-        with open(self.name, 'w', encoding='utf-8') as f:
-            f.write('')
+    def write(self, littleName="", content=""):
+        name = self.name + littleName + ".txt"
+        with open(name, 'a', encoding='utf-8') as f:
+            f.write(content)
             f.close()
 
-    def addWrite(self, string):
-        with open(self.name, 'a', encoding='utf-8') as f:
-            f.write(string)
-            f.close()
+    def cheakFileExists(self, littleName="", ):
+        name = self.name + littleName + ".txt"
+        if not os.path.exists(name):
+            with open(name, 'w', encoding='utf-8') as f:
+                f.write('')
+                f.close()
+            return False
+        return True
 
 
 # 爬虫类
 class NovelCrawler:
     # 初始化，传入基地址
-    def __init__(self, baseUrl, addWrite):
+    def __init__(self, menuUrl, baseUrl):
         # base链接地址
         self.baseURL = baseUrl
-
-        self.addWrite = addWrite
+        self.menuUrl = menuUrl
+        self.txtWriter = TxtWriter("转生成蜘蛛又怎样")
         # 每个章节的地址
-        self.pagesList = []
+        self.pagesList = {}
+        self.listIndex = []
 
-    # 获取所有的章节内容
+    # 待优化
+    def getNameFormChinese(self, string=""):
+        arr = string.split(" ")
+        value = ""
+        if arr:
+            value = arr[0]
+        if value not in self.listIndex:
+            self.listIndex.append(value)
+            return "s" + str(len(self.listIndex))
+        index = self.listIndex.index(value)
+        return "s" + str(index + 1)
+
     def setMenu(self):
         try:
             # 构建URL
-            url = self.baseURL
+            url = self.menuUrl
             request = urllib.request.Request(url)
             response = urllib.request.urlopen(request)
             # 返回UTF-8格式编码内容
-            string = str(response.read().decode("utf-8", "ignore"))
-            result = re.findall(r"<li><a href=\"/book/.+\">", string)
+            string = str(response.read().decode("gbk"))
+            result = re.findall(r"<li><a href=\"/book/(.+)\">(.+)</a></li>", string)
             if result:
                 for i in result:
-                    page = re.findall(r"\d{7,8}.html", i)
-                    if len(page) >= 1:
-                        self.pagesList.append(page[0])
+                    if i[1] and i[1].find("第") != -1:
+                        key = self.getNameFormChinese(i[1])
+                        if key not in self.pagesList.keys():
+                            self.pagesList[key] = []
+                        self.pagesList[key].append(i)
+            print(self.pagesList)
         # 无法连接，报错
         except urllib.request.URLError as e:
             if hasattr(e, "reason"):
@@ -56,44 +77,68 @@ class NovelCrawler:
                 return None
 
     # 传入章节的地址，获取章节的内容
-    def getPage(self, pageUrl):
+    def getPage(self, pageUrl, littleName, page=1):
         try:
             # 构建URL
-            url = self.baseURL + '/' + pageUrl
+            url = self.baseURL
+            if page == 1:
+                url += pageUrl
+            else:
+                url += pageUrl.split(".")[0] + "_" + str(page) + "." + pageUrl.split(".")[1]
+            print(url)
             request = urllib.request.Request(url)
             response = urllib.request.urlopen(request)
             # 返回UTF-8格式编码内容
             string = response.read().decode("gbk")
             titles = re.findall(r"<h1>.+</h1>", string)
+            title = ""
             if len(titles) >= 1:
                 title = titles[0]
                 title = re.sub("<h1>", "\n", title)
                 title = re.sub("</h1>", "", title)
-                self.addWrite(title)
+                title += "\n"
             result = re.findall(r"<p>.+</p>", string)
             allS = ""
+            next = False
             for i in result:
+                if i.find("（继续下一页）") != -1:
+                    next = True
                 item = i
                 item = re.sub("<p>", "", item)
                 item = re.sub("</p>", "\n", item)
                 item = re.sub("铅笔小说", "\n", item)
-                if item.find("（继续下一页）") == -1:
-                    allS += item
-            self.addWrite(allS)
+                item = re.sub("<p style=\"font-weight: 400;color:#af888c;\">（继续下一页）", "", item)
+                allS += item
+            self.txtWriter.write(littleName, title)
+            self.txtWriter.write(littleName, allS)
+            if next:
+                self.getPage(pageUrl, littleName, page + 1)
 
-        # 无法连接，报错
+                # 无法连接，报错
         except urllib.request.URLError as e:
             if hasattr(e, "reason"):
                 print(u"连接失败,错误原因", e.reason)
-                return None
+                if e.reason.find("Time-out") != -1:
+                    print("超时重新连接")
+                    self.getPage(pageUrl, littleName, page)
 
     def start(self):
         self.setMenu()
         try:
-            print("该帖子共有" + str(len(self.pagesList)) + "章节")
-            for i in range(6, 10):
-                print("正在写入 " + self.pagesList[i])
-                self.getPage(self.pagesList[i])
+            print("该帖子共有" + str(len(self.pagesList.keys())) + "卷")
+            for key in self.pagesList.keys():
+                littleName = key
+                if self.txtWriter.cheakFileExists(littleName):
+                    print(self.txtWriter.name + littleName + "已存在跳过")
+                    continue
+                for item in self.pagesList[key]:
+                    self.txtWriter.write(littleName, item[1] + "\n")
+                start = 0
+                for item in self.pagesList[key]:
+                    start += 1
+                    print("正在写入" + item[1] + "         " + str(start) + "/" + str(len(self.pagesList[key])))
+                    self.getPage(item[0], littleName, 1)
+
         # 出现写入异常
         except IOError as e:
             print("写入异常，原因" + e.message)
@@ -101,8 +146,7 @@ class NovelCrawler:
             print("写入任务完成")
 
 
-txtWriter = TxtWriter("转生成蜘蛛又怎样")
-
 menuUrl = 'https://www.x23qb.com/book/5999/'
-novelCrawler = NovelCrawler(menuUrl, txtWriter.addWrite)
+baseUrl = 'https://www.x23qb.com/book/'
+novelCrawler = NovelCrawler(menuUrl, baseUrl)
 novelCrawler.start()
